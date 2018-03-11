fff10ac581a377e6d9c4521e9193492f41fa207f
Anton Prokopenko <anton.9090@bk.ru>
2018-03-11 20:15:13 +0300

    fix: edit notiifications-page script

--- app/js/pages/notifications-page.js
+++ app/js/pages/notifications-page.js
@@ -3,11 +3,16 @@
   function HTMLCollectionToArray (HTMLCollection) {
     return Array.prototype.slice.call(HTMLCollection)
   }
-  
+
   var textItems = HTMLCollectionToArray(document.getElementsByClassName('notifications__message-text'))
 
-  textItems.map(function(item, i) {
-    if(item.clientHeight > 72) {
+  textItems.map(function(item) {
+    
+    var elemHeight = parseInt(window.getComputedStyle(item).height)
+
+    var elemScrollHeight = item.scrollHeight
+
+    if(elemScrollHeight > elemHeight) {
       item.classList.add('notifications__message-text--withgradient')
     }
   })
